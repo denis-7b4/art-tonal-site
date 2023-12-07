f843618a4de7cb970506a64c77e69caa066f4e93
Denis Kilin <dkilin@appliedtech.ru>
2023-12-08 00:18:10 +0500

    Works, Services added. Complete adaptive layout.

---
+++ js/service.js
@@ -0,0 +1,46 @@
+let emailPhoneValid = false;
+let nameFieldValid = false;
+
+const emailButton = document.getElementById("email_btn");
+const phoneButton = document.getElementById("phone_btn");
+const emailPhoneField = document.getElementById("email_phone");
+const nameField = document.getElementById("name");
+const sendButton = document.getElementById("send_btn");
+
+
+emailButton.addEventListener("click", function() {
+  if (emailButton.checked) {
+    emailPhoneField.setAttribute("type", "email");
+    emailPhoneField.setAttribute("placeholder", "* E-mail");
+  }
+  isFormValid();
+})
+
+phoneButton.addEventListener("click", function(){
+  if (phoneButton.checked) {
+    emailPhoneField.setAttribute("type", "tel");
+    emailPhoneField.setAttribute("placeholder", "* Номер Вашего телефона");
+  }
+  isFormValid();
+})
+
+emailPhoneField.addEventListener("change", function() {
+  if (emailPhoneField.validity.valid) {
+    if (!emailPhoneField.validity.valueMissing) {
+      emailPhoneValid = true;
+    } else emailPhoneValid = false;
+  } else emailPhoneValid = false;
+  isFormValid();
+});
+nameField.addEventListener("change", function() {
+  if (!nameField.validity.valueMissing) {
+    nameFieldValid = true;
+  } else nameFieldValid = false;
+  isFormValid();
+})
+
+function isFormValid () {
+  if (emailPhoneValid && nameFieldValid) {
+    sendButton.disabled = false;
+  } else sendButton.disabled = true;
+}
