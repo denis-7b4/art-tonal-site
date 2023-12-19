2ce32ebc1158020190fa85912fb190ae70386760
Denis Kilin <dkilin@appliedtech.ru>
2023-12-19 15:51:34 +0500

    Minor changes

--- pages/action.php
+++ pages/action.php
@@ -4,9 +4,16 @@
 <title>Заказ оформлен</title></head>
 <body>
 <?php
+$whatPaint = $_POST['what_paint_btn'];
 echo 'Здравствуйте, ', htmlspecialchars($_POST['name']), '.<br/><br/>';
 echo '<b>Ваш заказ</b><br/>';
-echo 'Будем красить: ', $_POST['what_paint_btn'], '.<br/>';
+echo 'Будем красить: ';
+if ($whatPaint == 'walls') {
+  echo 'стены';
+} elseif ($whatPaint == 'furniture') {
+  echo 'мебель';
+} else echo 'неизвестный объект';
+echo '.<br/>';
 echo 'Способ связи: ', $_POST['email_phone'], ': ', $_POST['email_phone_field'], '<br/>';
 echo 'Комментарий к заказу: ', $_POST['comment'], '<br/><br/>';
 echo 'Мы с вами свяжемся в самое ближайшее время.<br/><br/>';
