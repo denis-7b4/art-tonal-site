8183f4b417b2d15c68f81b53cbd4c60eed3550df
Denis Kilin <dkilin@appliedtech.ru>
2023-12-20 16:28:28 +0500

    PHP version

--- pages/action.php
+++ pages/action.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
+<html lang="ru">
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <title>Заказ оформлен</title></head>
 <body>
 <?php
+echo 'PHP version: ', phpversion(), '<br/><br/>';
+
 $whatPaint = $_POST['what_paint_btn'];
 echo 'Здравствуйте, ', htmlspecialchars($_POST['name']), '.<br/><br/>';
 echo '<b>Ваш заказ</b><br/>';
@@ -12,7 +15,7 @@ if ($whatPaint == 'walls') {
   echo 'стены';
 } elseif ($whatPaint == 'furniture') {
   echo 'мебель';
-} else echo 'неизвестный объект';
+} else {echo 'неизвестный объект';}
 echo '.<br/>';
 echo 'Способ связи: ', $_POST['email_phone'], ': ', $_POST['email_phone_field'], '<br/>';
 echo 'Комментарий к заказу: ', $_POST['comment'], '<br/><br/>';
@@ -21,4 +24,3 @@ echo '<a href="/pages/service.html"><b><<< НАЗАД</b></a>';
 ?>
 </body>
 </html>
-
